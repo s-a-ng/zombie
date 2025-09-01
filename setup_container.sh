3f9850b298d0a03c5284440097099f9e20b493be
sang . gnas <61368081+s-a-ng@users.noreply.github.com>
2025-09-01 01:06:59 -0700

    Update setup_container.sh

--- setup_container.sh
+++ setup_container.sh
@@ -2,26 +2,26 @@
 
 mkdir -p Startup
 
-cat <<EOF > Startup/install.bat
+cat <<'EOF' > Startup/install.bat
 @echo off
 mkdir C:\data
 cd C:\data
 
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol=3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 
 choco install python -y
 call refreshenv
 
 pip install websockets pymem psutil pywin32 aiohttp pyautogui
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.roblox.com/download/client?os=win', 'RobloxPlayerInstaller.exe')"
-start /wait "" "RobloxPlayerInstaller.exe"
 
 echo @echo off > "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run_bot_node.bat"
+echo cd /d C:\data >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run_bot_node.bat"
+echo powershell -NoProfile -ExecutionPolicy Bypass -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.roblox.com/download/client?os=win','RobloxPlayerInstaller.exe')" >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run_bot_node.bat"
+echo start /wait "" "RobloxPlayerInstaller.exe" >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run_bot_node.bat"
 echo start "" "python" "C:\bot-pool\Node\Node.py" >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run_bot_node.bat"
 echo exit >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run_bot_node.bat"
 
-powershell -Command "Set-MpPreference -DisableRealtimeMonitoring \$true; Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart; Restart-Computer -Force"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-MpPreference -DisableRealtimeMonitoring $true; Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart; Restart-Computer -Force"
 EOF
 
 git clone https://s-a-ng:${PAT}@github.com/s-a-ng/bot-pool.git
