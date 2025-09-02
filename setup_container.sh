232f54fac03065d5fc006d734c25ff5561c968bd
sang . gnas <61368081+s-a-ng@users.noreply.github.com>
2025-09-01 22:38:10 -0700

    Update setup_container.sh

--- setup_container.sh
+++ setup_container.sh
@@ -21,7 +21,7 @@ echo start /wait "" "RobloxPlayerInstaller.exe" >> "%APPDATA%\Microsoft\Windows\
 echo start "" "python" "C:\bot-pool\Node\Node.py" >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run_bot_node.bat"
 echo exit >> "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\run_bot_node.bat"
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-MpPreference -DisableRealtimeMonitoring $true; Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart; Restart-Computer -Force"
+powershell -Command "Set-MpPreference -DisableRealtimeMonitoring:1; Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart; Stop-Computer -Force"
 EOF
 
 git clone https://s-a-ng:${PAT}@github.com/s-a-ng/bot-pool.git
